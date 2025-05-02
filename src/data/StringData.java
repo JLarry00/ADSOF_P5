517349af5ca4fc38839ee3e848f64e3cbb5a886f
JLarry00 <juanlarrondofdez@gmail.com>
2025-05-02 13:26:43 +0200

    nada

--- src/data/StringData.java
+++ src/data/StringData.java
@@ -6,7 +6,7 @@ public class StringData {
     private String result;
 
     public StringData(String word, int times) {
-        if (times < 0) throw new IllegalArgumentException("Times must be greater than 0");
+        if (times < 0) throw new IllegalArgumentException("Times must be equal to or greater than 0");
         this.word = word;
         this.times = times;
         this.result = "";
